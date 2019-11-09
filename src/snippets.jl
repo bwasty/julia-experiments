119fc6f7569da0ed0391e6ff1cc4116b96dcdd8d
Benjamin Wasty <benny.wasty@gmail.com>
2019-11-09 16:35:45 +0100

    'old' nb changes

---
+++ src/snippets.jl
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:light
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Julia 1.2.0
+#     language: julia
+#     name: julia-1.2
+# ---
+
+f′(x) = 42 # \prime
+f′(2)
+
+a′ = 4
+
+n₂ = 4 # \_2
+
+n⁰ = 23 # \^0
+
+aₙ = 0 # \_n
+aⁿ = 1
+
+?methodswith
+
+# + {"jupyter": {"outputs_hidden": true}}
+
